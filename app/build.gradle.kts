f95950a1a8424693ee4e2934d7d9eba01f72da33
JessySeo9955 <jessy.seo.9955@gmail.com>
2025-05-31 23:06:49 -0400

    lamda error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
 	/////////////////////////////////////// AWS LAMBDA //////////////////////////////////////////////////////////////////////////////////////////
    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
     implementation("com.amazonaws:aws-lambda-java-events:3.11.1")
-	implementation("com.amazonaws:aws-lambda-java-events:3.11.0")
     runtimeOnly("com.amazonaws:aws-lambda-java-log4j2:1.5.1")
  
 
